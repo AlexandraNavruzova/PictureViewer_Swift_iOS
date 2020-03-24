fe2ab8cfa01ab9e815f9305708fca6fabe2eb0de
alexandranavruzova <haughtygrumbler@gmail.com>
2020-03-24 22:07:47 +0300

    pictureViewer

---
+++ pictureViewer/DetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.swift
+//  pictureViewer
+//
+//  Created by Александра Наврузова on 23.03.2020.
+//  Copyright © 2020 Alexandra Navruzova. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
